048710865facc4c7670d200e4571c4bf7dc8df0a
wangxiaoyang <15847424609@163.com>
2021-01-20 12:57:17 +0800

    build: 升级element-plus，vue-router，vite依赖版本

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-module.exports = {
+import vue from '@vitejs/plugin-vue'
+
+export default {
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true
+            }
+        },
+        hmr: {
+            overlay: false
+        }
+    },
     optimizeDeps: {
         include: [
             'element-plus/lib/locale/lang/zh-cn',
             'dayjs/locale/zh-cn.js',
             'dayjs'
         ]
-    },
-    proxy: {
-        '/api': {
-            target: 'http://localhost:8080'
-        }
     }
 }
