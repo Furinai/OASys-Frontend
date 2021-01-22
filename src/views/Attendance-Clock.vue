46722dee44227b00e9462a633d8ad495fe532c19
wangxiaoyang <15847424609@163.com>
2021-01-23 00:01:47 +0800

    feat: 新增单独的打卡页面

---
+++ src/views/Attendance-Clock.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="text-center">
+        <div class="current-time">{{ currentTime }}</div>
+        <div class="current-date">{{ currentDate }}</div>
+        <div class="clock-button">
+            <el-button-group>
+                <el-button type="primary" icon="el-icon-arrow-left"
+                           @click="clockIn" :disabled="Boolean(attendance)">
+                    签到
+                </el-button>
+                <el-button type="primary" @click="clockOut"
+                           :disabled="Boolean(!attendance) || Boolean(attendance.clockOutTime)">
+                    签退
+                    <i class="el-icon-arrow-right el-icon--right"></i>
+                </el-button>
+            </el-button-group>
+        </div>
+    </div>
+</template>
+
+<script>
+import {clockIn, clockOut, getAttendances} from '/@/utils/api'
+import {mapState} from 'vuex'
+import dayjs from 'dayjs'
+
+export default {
+    name: "Attendance-Clock",
+    data() {
+        return {
+            attendance: {},
+            currentTime: dayjs().format('HH:mm'),
+            currentDate: dayjs().format('YYYY年MM月DD日')
+        }
+    },
+    created() {
+        this.getAttendances()
+    },
+    computed: mapState([
+        "auth"
+    ]),
+    methods: {
+        getAttendances() {
+            let date = new Date()
+            let year = date.getFullYear(), month = date.getMonth() + 1, day = date.getDate()
+            getAttendances({userId: this.auth.id, year, month, day}).then(result => {
+                if (result.code === '0000') {
+                    this.attendance = result.data[0]
+                }
+            })
+        },
+        clockIn() {
+            clockIn({userId: this.auth.id}).then(result => {
+                if (result.code === '0000') {
+                    this.attendance = result.data
+                    let minutes = this.attendance.comeLateMinutes
+                    if (minutes) {
+                        this.$message.success('签到成功，迟到' + minutes + '分钟')
+                    } else {
+                        this.$message.success('签到成功')
+                    }
+                }
+            })
+        },
+        clockOut() {
+            clockOut({id: this.attendance.id}).then(result => {
+                if (result.code === '0000') {
+                    this.attendance = result.data
+                    let minutes = this.attendance.leaveEarlyMinutes
+                    if (minutes) {
+                        this.$message.success('签退成功，早退' + minutes + '分钟')
+                    } else {
+                        this.$message.success('签退成功')
+                    }
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+.current-time {
+    margin-top: 40px;
+    font-size: 50px;
+}
+
+.current-date {
+    margin: 20px 0;
+}
+
+.clock-button {
+    margin-top: 100px;
+}
+</style>
