1141ded160c4741612fc335e34ffd422ef074723
wangxiaoyang <15847424609@qq.com>
2022-02-12 23:35:37 +0800

    :bug: 修正el-menu-item组件名称

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -7,15 +7,14 @@
                     <i class="el-icon-house"></i>
                     <template #title>首页</template>
                 </el-menu-item>
-                <el-submenu v-for="(category, i) in permissions" :index="i + ''">
+                <el-sub-menu v-for="(category, i) in permissions" :index="i + ''">
                     <template #title>
                         <i v-if="category.icon" :class="category.icon"/>{{ category.name }}
                     </template>
-                    <el-menu-item v-for="(menu, j) in category.children"
-                                  :index="i + '-' + j" :route="{name: menu.routerName}">
+                    <el-menu-item v-for="(menu, j) in category.children" :index="i + '-' + j" :route="{name: menu.routerName}">
                         <i v-if="menu.icon" :class="menu.icon"/>{{ menu.name }}
                     </el-menu-item>
-                </el-submenu>
+                </el-sub-menu>
             </el-scrollbar>
         </el-menu>
     </div>
